56705207457cd994982bae4d33ee6fd2aaeb6013
kasemsanm <32940983+Kasemsanm@users.noreply.github.com>
2018-02-11 00:38:25 +0700

    reverb

--- src/app/app.firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyCC_Dtt2Kf95xbsn1GOWK1d8PwSuZJGYig",
-    authDomain: "fttxplanner.firebaseapp.com",
-    databaseURL: "https://fttxplanner.firebaseio.com",
-    projectId: "fttxplanner",
-    storageBucket: "fttxplanner.appspot.com",
-    messagingSenderId: "746716007871"
-  };
